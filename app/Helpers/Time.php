22d9a0fcd8c77d1e732bcbaaa391bf6399a899f2
Unknown <szymon.gackowski@gmail.com>
2018-04-18 15:35:43 +0200

    Improved description

--- app/Helpers/Time.php
+++ app/Helpers/Time.php
@@ -5,9 +5,12 @@ namespace App\Helpers;
 class Time{
 	
 	/**
-	 * Porównuje dwie godziny i zwraca wynik -1/0/1
-	 * @param  string $time1 [description]
-	 * @param  string $time2 [description]
+	 * Porównuje dwie godziny i zwraca wynik -1/0/1.
+	 * -1 - pierwsza godzina jest wcześniejsza niż druga ($time1 mniejsze)
+	 *  0 - dwie godziny są równe
+	 *  1 - druga godzina jest wcześniejsza niż pierwsza ($time1 większe)
+	 * @param  string $time1 godzina w formacie HH:mm
+	 * @param  string $time2 godzina w formacie HH:mm
 	 * @return Integer        [description]
 	 */
 	public static function compare($time1, $time2){
